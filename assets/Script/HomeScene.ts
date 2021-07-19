2168b7d85cdf56571c703e95592fac3079175d7a
ForwardTheDream <330415890@qq.com>
2021-07-19 16:32:05 +0800

    【提交用途】主场景小鸟移动说话功能展示 【提交人】鲁定国

--- assets/Script/HomeScene.ts
+++ assets/Script/HomeScene.ts
@@ -1,12 +1,20 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class Helloworld extends cc.Component {
+export default class HomeScene extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
 
     start () {
         
+    }
+
+    update () {
+
+    }
+
+    onDestroy() {
+
     }
 }
---
+++ assets/Script/Object/Bird.ts
@@ -0,0 +1,37 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+        this.node.runAction(
+            cc.sequence(
+                cc.moveTo(3, new cc.Vec2(200, 400)),
+                cc.delayTime(2),
+                cc.callFunc(() => {
+                    // ...
+                    this.talk();
+                })
+            )
+        );
+    }
+
+    update (dt) {
+
+    }
+
+    talk () {
+        console.log("小鸟开始说话了");
+    }
+}
